fbb93980d04fbbac6f0bbbe823f93ec03aa16706
Ramón Tomás <ramon.tomas84@hotmail.com>
2018-05-24 14:02:42 +0200

    Created scaffolding

---
+++ DemoApps/WebApi/Controllers/BeersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Infrastructure;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BeersController : Controller
+    {
+        private readonly BeersContext ctx;
+
+        public BeersController(BeersContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        // GET api/beers
+        [HttpGet]
+        public IEnumerable<Beer> Get()
+        {
+            return ctx.Beers.ToList();
+        }        
+    }
+}
---
+++ DemoApps/WebApi/Infrastructure/BeersContextSeed.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Infrastructure
+{
+    public class BeersContextSeed
+    {
+        public async Task SeedAsync(BeersContext context, ILogger<BeersContextSeed> logger)
+        {
+            var policy = CreatePolicy(logger, nameof(BeersContextSeed));
+
+            await policy.ExecuteAsync(async () =>
+            {
+                if (!context.Beers.Any())
+                {
+                    await context.Beers.AddRangeAsync(GetDefaultBeers());
+
+                    await context.SaveChangesAsync();
+                }
+            });
+        }
+
+        private IEnumerable<Beer> GetDefaultBeers()
+        {
+            return new[]
+            {
+                new Beer()
+                {
+                    Name = "Guinness",
+                    Country = "Ireland",
+                    Code = "guinness"
+                },
+                new Beer()
+                {
+                    Name = "Blue Moon",
+                    Country = "Belgium",
+                    Code = "blue_moon"
+                },
+                new Beer()
+                {
+                    Name = "Dos Equis",
+                    Country = "Mexico",
+                    Code = "dos_equis"
+                },
+                new Beer()
+                {
+                    Name = "Fat Tire",
+                    Country = "Belgium",
+                    Code = "fat_tire"
+                },
+                new Beer()
+                {
+                    Name = "Heady Topper",
+                    Country = "US",
+                    Code = "heady_topper"
+                },
+                new Beer()
+                {
+                    Name = "Foster's Lager",
+                    Country = "Australia",
+                    Code = "foster's_lager"
+                }
+            };
+        }       
+
+        private Policy CreatePolicy(ILogger<BeersContextSeed> logger, string prefix, int retries = 3)
+        {
+            return Policy.Handle<SqlException>()
+                .WaitAndRetryAsync(
+                    retryCount: retries,
+                    sleepDurationProvider: retry => TimeSpan.FromSeconds(5),
+                    onRetry: (exception, timeSpan, retry, ctx) =>
+                    {
+                        logger.LogTrace($"[{prefix}] Exception {exception.GetType().Name} with message ${exception.Message} detected on attempt {retry} of {retries}");
+                    }
+                );
+        }    
+    }
+}
