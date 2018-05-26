ebbbdc7cc28e235b6ca561645dd53b6c398c675a
Ramón Tomás <rtomas@plainconcepts.com>
2018-05-26 08:37:23 +0200

    Created demo for netfx 4.7

---
+++ DemoApps/Webapp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webapp.Startup))]
+namespace Webapp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
